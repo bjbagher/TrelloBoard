a9200059792a3c2a00101b4bd9b3089167952177
bb <bjbagher@gmail.com>
2020-07-01 18:47:14 -0400

    add amplify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom"
 import App from "./App.js"
 import { RecoilRoot } from "recoil"
 import "./index.css"
+import 'regenerator-runtime/runtime'
 
 
 const root = document.getElementById("root")
