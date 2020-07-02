7ea4371d25d3538bb4a94c5184e253b805a32557
bb <bjbagher@gmail.com>
2020-07-01 21:37:02 -0400

    added oauth

--- src/App.js
+++ src/App.js
@@ -12,6 +12,46 @@ import { BrowserRouter as Router, Link, Switch } from "react-router-dom"
 
 // Amplify.configure(aws_exports);
 
+
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+class Landing extends Component {
+    state = { user: null, customState: null };
+
+    componentDidMount() {
+        Hub.listen("auth", ({ payload: { event, data } }) => {
+            switch (event) {
+                case "signIn":
+                    this.setState({ user: data });
+                    break;
+                case "signOut":
+                    this.setState({ user: null });
+                    break;
+                case "customOAuthState":
+                    this.setState({ customState: data });
+            }
+        });
+
+        Auth.currentAuthenticatedUser()
+            .then(user => this.setState({ user }))
+            .catch(() => console.log("Not signed in"));
+    }
+
+    render() {
+        const { user } = this.state;
+
+        return (
+            <div className="App">
+                <button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}>Open Facebook</button>
+                <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>Open Google</button>
+                <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+                <button onClick={() => Auth.signOut()}>Sign Out {"a"}</button>
+            </div>
+        );
+    }
+}
 // class Landing extends Component {
 //     render() {
 //         return (
@@ -269,8 +309,8 @@ function App() {
                         <Board />
                     </ Router >
                     <Router path="/">
-                        <Board />
-                        {/* <Landing /> */}
+                        {/* <Board /> */}
+                        <Landing />
                     </Router>
                 </Switch>
             </Router>
