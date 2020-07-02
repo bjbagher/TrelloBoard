24fe5a2c83a21ccd8766f6617096b7b5b58c872a
bb <bjbagher@gmail.com>
2020-07-01 23:15:46 -0400

    add cognito

--- src/App.js
+++ src/App.js
@@ -14,8 +14,83 @@ import { BrowserRouter as Router, Link, Switch } from "react-router-dom"
 
 
 import Amplify, { Auth, Hub } from 'aws-amplify';
-// import awsconfig from './aws-exports';
-// Amplify.configure(awsconfig);
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
+
+
+// import React, { Component } from 'react';
+// import { Auth } from 'aws-amplify';
+// To federated sign in from Google
+class SignInWithGoogle extends Component {
+    constructor(props) {
+        super(props);
+        this.signIn = this.signIn.bind(this);
+    }
+
+    componentDidMount() {
+        const ga = window.gapi && window.gapi.auth2 ?
+            window.gapi.auth2.getAuthInstance() :
+            null;
+        if (!ga) this.createScript();
+    }
+
+    signIn() {
+        const ga = window.gapi.auth2.getAuthInstance();
+        ga.signIn().then(
+            googleUser => {
+                this.getAWSCredentials(googleUser);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
+
+    async getAWSCredentials(googleUser) {
+        const { id_token, expires_at } = googleUser.getAuthResponse();
+        const profile = googleUser.getBasicProfile();
+        let user = {
+            email: profile.getEmail(),
+            name: profile.getName()
+        };
+
+        const credentials = await Auth.federatedSignIn(
+            'google',
+            { token: id_token, expires_at },
+            user
+        );
+        console.log('credentials', credentials);
+    }
+
+    createScript() {
+        // load the Google SDK
+        const script = document.createElement('script');
+        script.src = 'https://apis.google.com/js/platform.js';
+        script.async = true;
+        script.onload = this.initGapi;
+        document.body.appendChild(script);
+    }
+
+    initGapi() {
+        // init the Google SDK client
+        const g = window.gapi;
+        g.load('auth2', function () {
+            g.auth2.init({
+                client_id: 'your_google_client_id',
+                // authorized scopes
+                scope: 'profile email openid'
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.signIn}>Sign in with Google</button>
+            </div>
+        );
+    }
+}
 
 class Landing extends Component {
     state = { user: null, customState: null };
@@ -44,6 +119,7 @@ class Landing extends Component {
 
         return (
             <div className="App">
+                <a href="https://trelloboard.auth.us-east-2.amazoncognito.com/login?client_id=31svjcov3lav0pqkd28hqpho0o&response_type=token&scope=email+openid+profile&redirect_uri=https://master.d2dbrifwdknjod.amplifyapp.com">Google Signin</a>
                 <button onClick={() => Auth.federatedSignIn({ provider: 'Facebook' })}>Open Facebook</button>
                 <button onClick={() => Auth.federatedSignIn({ provider: 'Google' })}>Open Google</button>
                 <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
@@ -311,6 +387,8 @@ function App() {
                     </ Router >
                     <Router path="/">
                         {/* <Board /> */}
+                        <SignInWithGoogle />
+                        <p>*************************************</p>
                         <Landing />
                     </Router>
                 </Switch>
